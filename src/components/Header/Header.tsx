8075078891a7cbb5344d24f649f25ee47c5c4c16
Mateusz <mateuszb430@gmail.com>
2023-11-27 13:11:15 +0100

    added header, menu nad students list

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import "./Header.css"
+import {AvailableStudentsPage} from "../AvailableStudentsPage/AvailableStudentsPage";
+
+
+export const Header = () => {
+
+
+    return <>
+        <div id="header">
+            <div id="header-inside">
+
+                <img id="logo" src="logo.webp" alt="megaK"/>
+
+                <div id="user">Tester Testowy</div>
+
+            </div>
+        </div>
+
+
+    </>
+}
